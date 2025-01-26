47a0c21377c41ab41df96144b748347bef134b2f
Alexandr Gromov <gromovaleksandr813@gmail.com>
2025-01-26 14:44:24 +0300

    Initial

---
+++ kurs_project_prodvinutoe_program_INI_file/kurs_project_prodvinutoe_program_INI_file.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Ini.h"
+
+int main() {
+	try {
+	ini_parser parser("ini.ini");
+	auto value = parser.get_value <std::string> ("topsecret.server.com.ForwardX11");
+	std::cout  << value << std::endl;
+	
+	for (auto i : parser.errors) {
+	std::cout << "ERRORS" << std::endl;
+	
+		std::cout << i << std::endl; // вывод списка ошибок
+		
+		std::cout << std::endl;
+	}
+	} catch (const std::exception& ex) { std::cout << ex.what() << std::endl; }
+	
+	return 0;
+}
+
